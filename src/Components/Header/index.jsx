d20df230ac5779bc3e72dd49c8c078ad2f26cb5c
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-01-20 00:17:15 -0300

    :construction: Header em construção

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import S from "./style.module.scss";
 
 export default function Header() {
   return (
-    <header>
+    <header className={S}>
       <h1>&lt;Alan/&gt;</h1>
-      <nav>
+      <nav className={S["nav-container"]}>
         <Link to="/">Home</Link>
         <Link to="/about-me">Sobre mim</Link>
         <Link to="/certifications">Certificados</Link>
