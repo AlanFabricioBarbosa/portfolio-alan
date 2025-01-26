f4ea58b37b5ced1da14301f6e44441a3b9972b98
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-01-25 22:48:45 -0300

    :sparkles: feat: Página Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,28 @@
 import { AnimatedBackground } from "animated-backgrounds";
+import S from "./style.module.scss";
 
 export default function Home() {
   return (
-    <div>
+    <section>
       <AnimatedBackground animationName="starryNight" blendMode="normal" />
-    </div>
+      <section className={S["home_text_container"]}>
+        <div className={S["home_text_content"]}>
+          <h2>
+            {Array.from("Olá, sou Alan Fabrício!").map((char, index) => (
+              <span
+                key={index}
+                style={{
+                  marginRight: char === " " ? "1rem" : "0",
+                }}
+              >
+                {char}
+              </span>
+            ))}
+          </h2>
+          <p>Bem-vindo ao meu portfólio!</p>
+          <p>Sinta-se à vontade para explorá-lo.</p>
+        </div>
+      </section>
+    </section>
   );
 }
