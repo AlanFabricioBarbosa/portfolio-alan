918e5eb981e9a71af96f8df704d0d4579ee6f128
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-01-30 18:47:16 -0300

    :books: Comentando estágio do projeto no readme

--- src/pages/Certifications/index.jsx
+++ src/pages/Certifications/index.jsx
@@ -1,9 +1,20 @@
 import CardEducation from "../../Components/CardEducations";
+import S from "./style.module.scss";
 
 export default function Certifications() {
   return (
     <>
-      <CardEducation />
+      <section>
+        <div className={S["text-certifications-container"]}>
+          <h2>Certificações</h2>
+          <p>
+            Confira minhas formações como programador! Aqui estão os cursos e
+            certificações que moldaram minha jornada e mantêm minhas habilidades
+            sempre afiadas
+          </p>
+        </div>
+        <CardEducation />
+      </section>
     </>
   );
 }
